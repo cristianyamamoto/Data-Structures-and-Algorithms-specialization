077dc5b6e2fa19d0381f8e8fbb8a56db822b2b6d
Cristian Yamamoto <yamamotocristian@gmail.com>
2020-09-24 23:26:32 -0300

    Updating with some content from module 3

---
+++ 1 - Algorithmic Toolbox/week3_greedy_algorithms/mysolutions/change.py
@@ -0,0 +1,19 @@
+# Uses python3
+import sys
+
+def get_change(m):
+    count = 0
+    coins = [10, 5, 1]
+    n = len(coins)
+    ans = []
+    for i in range(0, n):
+        while(m >= coins[i]):
+            m -= coins[i]
+            ans.append(coins[i])
+            count += 1
+    #print(ans)
+    return count
+
+if __name__ == '__main__':
+    m = int(sys.stdin.read())
+    print(get_change(m))
